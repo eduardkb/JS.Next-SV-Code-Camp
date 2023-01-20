fc0caf8d038acfd7ef44ab6790fe9179a85ee23c
eduardkb <eduardbuhali@gmail.com>
2023-01-20 15:36:17 -0300

    02e24-Tramsfered context logic to a exclusive file called ThemeContext.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,24 +1,27 @@
-import React, { createContext, useState } from "react";
-
-// create the context for the theme state
-// and it is exported because it needs to be available in other components
-export const ThemeContext = createContext();
+import React, { useContext } from "react";
+import { ThemeContext, ThemeProvider } from "../contexts/ThemeContext";
 
 function Layout({ startingTheme, children }) {
-  const [theme, setTheme] = useState(startingTheme);
   return (
-    <ThemeContext.Provider value={{ setTheme, theme }}>
-      <div
-        className={
-          theme === "light" ? "container-fluid light" : "container-fluid dark"
-        }
-      >
-        {/* // children is a standart param for all components
+    <ThemeProvider startingTheme={startingTheme}>
+      <LayoutThemeProvider>{children}</LayoutThemeProvider>
+    </ThemeProvider>
+  );
+}
+
+function LayoutThemeProvider({ startingTheme, children }) {
+  const { theme } = useContext(ThemeContext);
+  return (
+    <div
+      className={
+        theme === "light" ? "container-fluid light" : "container-fluid dark"
+      }
+    >
+      {/* // children is a standart param for all components
         // it renders all the components that
         // this component wraps */}
-        {children}
-      </div>
-    </ThemeContext.Provider>
+      {children}
+    </div>
   );
 }
 
